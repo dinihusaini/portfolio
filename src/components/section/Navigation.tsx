1639336639ebcf8c188f6679567ef09d359fdc82
Dini Husaini <dinihusaini2312@gmail.com>
2026-06-10 15:00:44 +0700

    Update Navigation.tsx

--- src/components/section/Navigation.tsx
+++ src/components/section/Navigation.tsx
@@ -114,8 +114,8 @@ const Navigation = () => {
             WebkitTextFillColor: themeColors.colors.pink[500]
           }}
           onClick={() => window.location.href = '/'}
-          aria-label="Your Name - Go to homepage">
-          Your Name
+          aria-label="Dini Husaini - Go to homepage">
+          Dini Husaini
         </button>
         
         {/* Desktop Navigation */}
